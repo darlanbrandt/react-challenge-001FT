243874a31c8cbf92fe7152afe3b88c5ae895c517
darlanbrandt <darlanbrandt@gmail.com>
2021-09-01 04:58:17 -0300

    Adjusts Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,28 +6,24 @@ export default function Hero() {
   return (
     <>
       <div className={css.hero}>
-        <div class="hero-text">
-          <div class="hero-title">
-            <h1 class="main-title title">
-              Easily create or join a local nanny share with Hapu
-            </h1>
+        <div className={css.text}>
+          <div className={css.title}>
+            <h1>Easily create or join a local nanny share with Hapu</h1>
             <p>
               Hapu is Airbnb for nanny share. Share your home, nanny and costs
               and create new flexible, affordable solutions in childcare.
             </p>
-            <div class="play">
-              <a href="">
+            <div className={css.play}>
+              <a href="#">
                 <img src={playButton} alt="Play button" />
-              </a>
-              <a href="">
                 <span>See hapu in action (27 seconds)</span>
               </a>
             </div>
           </div>
         </div>
-      </div>
-      <div class="hero-image">
-        <img src="" alt="" />
+        <div className={css.image}>
+          <img src="" alt="" />
+        </div>
       </div>
     </>
   );
