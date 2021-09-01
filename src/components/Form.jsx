fd2d592453477f05cb162e4d247ca0106b150aa0
Darlan Brandt <darlanbrandt@users.noreply.github.com>
2021-09-01 09:10:57 -0300

    Form component (#10)

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,8 +3,8 @@ import css from 'components/styles/Form.module.css';
 
 export default function Form() {
   return (
-    <div className={css.form}>
-      <form action>
+    <div className={css.container}>
+      <form action="POST" className={css.form}>
         <div className={css.inputFields}>
           <input type="text" name="" id="" placeholder="Your name" />
           <input type="text" name="" id="" placeholder="Your email" />
