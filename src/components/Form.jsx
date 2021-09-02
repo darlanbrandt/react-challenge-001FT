e2ac6ad6a999cc84185ca7c31f85165a6a2674a5
darlanbrandt <darlanbrandt@gmail.com>
2021-09-01 21:22:52 -0300

    Implements form functionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from 'services/api';
 import css from 'components/styles/Form.module.css';
 
 export default function Form() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(name);
+  };
+
   return (
     <div className={css.container}>
-      <form action="POST" className={css.form}>
+      <form onSubmit={handleSubmit} className={css.form}>
         <div className={css.inputFields}>
-          <input type="text" name="" id="" placeholder="Your name" />
-          <input type="text" name="" id="" placeholder="Your email" />
+          <label htmlFor="name" className={css.scrOnly}>
+            Enter your name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            placeholder="Your name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label htmlFor="email" className={css.scrOnly}>
+            Enter your e-mail
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            placeholder="Your email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-        <button type="button" class="button">
+        <button type="submit" className={css.button}>
           Send
         </button>
       </form>
